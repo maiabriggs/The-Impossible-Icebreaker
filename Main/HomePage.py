2053b44dcf819bac0626fc2acfd89ead6036b75d
Maia <maia@briggs.nz>
2022-02-05 19:31:32 +0000

    Added a begin button

--- Main/HomePage.py
+++ Main/HomePage.py
@@ -1,6 +1,23 @@
 import tkinter as tk
+from PIL import Image
 window=tk.Tk()
-
 window.title("The Impossible Icebreaker")
-window.geometry("300x200+10+20")
+
+message = tk.Label(window, text = "The Impossible Icebreaker")
+message.pack()
+
+def start():
+    print("Start")
+
+begin = tk.Button(
+    window, 
+    text = "Begin!",
+    command = start())
+
+begin.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
+
 window.mainloop()
