ecc0374963e475e73b7b77d2504307985e0a5ef6
paulinasg <53087467+paulinasg@users.noreply.github.com>
2022-02-05 21:37:06 +0000

    added comment d

--- Main/HTBIntro.py
+++ Main/HTBIntro.py
@@ -4,23 +4,27 @@ from PIL import ImageTk,Image
 def startGame():
     print("Game start")
 
+#setting up canvas with white background
 root = Tk()
 canvas = Canvas(root, width = 2000, height = 2000)
 canvas.config(bg="white")
 canvas.pack()  
+
+#open title image as Image, resize, then save as PhotoImage so its displayed in TKinter
 title = Image.open("/Users/paulinagerchuk/Documents/IMG_0398.jpg")
 title = title.resize((650, 350), Image.ANTIALIAS)
 title = ImageTk.PhotoImage(title)
+
+#open, resize, and save buttonImage as a PhotoImage
 beginButtonImg = Image.open("/Users/paulinagerchuk/Documents/IMG_0399.jpg")
 beginButtonImg = beginButtonImg.resize((300, 150), Image.ANTIALIAS)
 beginButtonImg = ImageTk.PhotoImage(beginButtonImg)
+#make a button with the beginButtonImg image inside it
 beginButton = Button(root, image=beginButtonImg, command=startGame)
 beginButton.pack()
+
+#create objects of both images on canvas and choose their location
 canvas.create_image(400, 50, anchor=NW, image=title)
 canvas.create_window(550, 450, anchor=NW, window=beginButton)
 root.mainloop()
 
-
-#figure out placement
-#add button
-#change image when hovered over
