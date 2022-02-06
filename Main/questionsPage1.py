408f06bfeaaec9a9073e21b130eb2f8f6403a0d2
wietsdev <wholwerda2@gmail.com>
2022-02-06 00:15:23 +0000

    chosen question message gui

--- Main/questionsPage1.py
+++ Main/questionsPage1.py
@@ -6,13 +6,13 @@ path_to_qSet1 = "../Archie/TestQuestions.txt"
 window = tk.Tk()
 window.title("Learning how to use tkinter")
 
-message = tk.Label(window, text = "Testing tkinter")
-message.pack()
-
 with open(path_to_qSet1) as file:           #filename of question set 1 path variable at top
     questionsList = file.readlines()
 
 questionChosen = random.choice(questionsList)
 print(questionChosen)
+
+message = tk.Label(window, text = questionChosen)
+message.pack()
 
 window.mainloop()
