6ff6f31f95a926a51d0a7e1bf16d501d74b816e7
paulinasg <53087467+paulinasg@users.noreply.github.com>
2022-02-06 03:39:29 +0000

    ugly error 404 screen

---
+++ Main/HTB404Screen.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import random
+
+def startGame():
+    print("Start game")
+    
+root = Tk()
+canvas = Canvas(root, width = 2000, height = 2000)
+canvas.config(bg="#bdfaa3")
+canvas.pack()
+
+buttonImg = Image.open("/Users/paulinagerchuk/Documents/IMG_0408.jpg")
+buttonImg = buttonImg.resize((700, 450), Image.ANTIALIAS)
+buttonImg = ImageTk.PhotoImage(buttonImg)
+canvas.create_image(400, 150, anchor=NW, image=buttonImg)
+
+root.mainloop()
