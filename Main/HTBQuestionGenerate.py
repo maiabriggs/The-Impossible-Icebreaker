6479c847660e1e304ffe115be6a226d5c6a87380
paulinasg <53087467+paulinasg@users.noreply.github.com>
2022-02-06 00:56:50 +0000

    code to randomly pick question and have names as answers

---
+++ Main/HTBQuestionGenerate.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import random
+
+def startGame():
+    print("Start game")
+    
+path_to_qSet1 = "TestQuestions.txt"
+
+root = Tk()
+canvas = Canvas(root, width = 2000, height = 2000)
+canvas.config(bg="white")
+canvas.pack()
+
+root.title("Learning how to use tkinter")
+
+#filename of question set 1 path variable at top
+with open(path_to_qSet1) as file:
+    questionsList = file.read().splitlines()
+    
+question = random.choice(questionsList)
+
+print(question)
+
+#making name buttons
+#uploading names from file into variables
+namesFile = open("namesFile.txt","r")
+player1 = namesFile.readline()
+player2 = namesFile.readline()
+player3 = namesFile.readline()
+player4 = namesFile.readline()
+
+message = Label(root, height=2, font=("Arial", 20), text = question)
+message.pack()
+canvas.create_window(200, 100, anchor=NW, window=message)
+
+
+buttonImg = Image.open("/Users/paulinagerchuk/Documents/IMG_0401.jpg")
+buttonImg = buttonImg.resize((250, 120), Image.ANTIALIAS)
+buttonImg = ImageTk.PhotoImage(buttonImg)
+button = Button(root, image=buttonImg, command=startGame, text=player1, font=("Arial",20), compound=CENTER)
+button.pack()
+canvas.create_window(200, 300, anchor=NW, window=button)
+
+
+button = Button(root, image=buttonImg, command=startGame, text=player2, font=("Arial",20), compound=CENTER)
+button.pack()
+canvas.create_window(600, 300, anchor=NW, window=button)
+
+button = Button(root, image=buttonImg, command=startGame, text=player3, font=("Arial",20), compound=CENTER)
+button.pack()
+canvas.create_window(200, 500, anchor=NW, window=button)
+
+button = Button(root, image=buttonImg, command=startGame, text=player4, font=("Arial",20), compound=CENTER)
+button.pack()
+canvas.create_window(600, 500, anchor=NW, window=button)
+
+root.mainloop()
